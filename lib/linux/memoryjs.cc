5af9422a6ae021045037c462fe5236f2c0b46336
OhMyGuus <info@guus.ninja>
2021-03-24 02:58:15 +0100

    Fixed small mistake

--- lib/linux/memoryjs.cc
+++ lib/linux/memoryjs.cc
@@ -454,7 +454,7 @@ Napi::Value readBuffer(const Napi::CallbackInfo& args) {
   size_t size = args[2].As<Napi::Number>().Int64Value();
   char* data = Memory.readBuffer(handle, address, size);
   Napi::Buffer<char> buffer = Napi::Buffer<char>::New(env, data, size);
-  free(data);
+  // free(data);
   delete [] data; // deleting the char array...
   if (args.Length() == 4) {
     Napi::Function callback = args[3].As<Napi::Function>();
